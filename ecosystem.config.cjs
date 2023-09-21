f91b91f86564ab75c76e475f1d1948da115095ab
Elia Lazzari <elia.lazzari@gmail.com>
2023-09-21 15:24:38 +0200

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -16,10 +16,9 @@ module.exports = {
             ref: 'origin/main',
             repo: 'https://github.com/Elius94/virtual-gallery',
             path: '/opt/web/srv.eliusoutdoor.com/virtual-gallery',
-            'pre-deploy-local': '',
             'pre-deploy': 'git fetch --all',
             'post-deploy':
-                'whereis node && source /root/.bashrc && npm install && npm run build && pm2 reload ecosystem.config.js && pm2 save',
+                'whereis node && source /root/.bashrc && npm install',
             env: {
 
             },

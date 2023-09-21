bf1811b87f5f1ebd3e05941f6100395ecb29b944
Elia Lazzari <elia.lazzari@gmail.com>
2023-09-21 16:37:58 +0200

    retry

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,5 +7,21 @@ module.exports = {
             PM2_SERVE_PORT: 3001,
             PM2_SERVE_SPA: 'true'
         }
-    }]
+    }],
+    deploy: {
+        production: {
+            user: 'root',
+            host: '194.163.139.154',
+            key: 'deploy.key',
+            ref: 'origin/main',
+            repo: 'https://github.com/Elius94/virtual-gallery',
+            path: '/opt/web/srv.eliusoutdoor.com/virtual-gallery',
+            'pre-deploy': 'git fetch --all',
+            'post-deploy':
+                'source /root/.bashrc && npm install && npm run build && pm2 reload ecosystem.config.cjs && pm2 save ',
+            env: {
+
+            },
+        },
+    },
 }

2db4f73ae9f630928df9137b9ba197c146fee9c0
Elia Lazzari <elia.lazzari@gmail.com>
2023-09-21 15:32:34 +0200

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -18,7 +18,7 @@ module.exports = {
             path: '/opt/web/srv.eliusoutdoor.com/virtual-gallery',
             'pre-deploy': 'git fetch --all',
             'post-deploy':
-                'source /root/.bashrc && npm install && node ./esbuild.js && npx seo-injector --build-dir public --pretty true --verbose true && pm2 reload ecosystem.config.cjs --env production',
+                'source /root/.bashrc && npm install && npm run build && pm2 reload ecosystem.config.js --env production && pm2 save',
             env: {
 
             },
